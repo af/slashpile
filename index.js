fa67ae85fd4d0aec3d99b94b8d3683a3bbd681d5
Aaron Franks <aaron.franks@gmail.com>
2016-01-20 21:30:17 -0800

    Light refactor of toy implementation

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const lineRegex = /^(\s+)(\w+)(\.(\w+))*/
 const commentRegex = /^\s+\//
 
-const create = (createEl) => {
-    return (templateChunks, values) => (config) => {        // TODO: rest params for values
-        const lines = templateChunks.join('$$$').split('\n')     // FIXME: join hack
-        const parsedLines = lines.map((line) => {
-            const match = lineRegex.exec(line)
-            if (!match || line.match(commentRegex)) return null
+// TODO: pass in "takeParam" function that pulls template params in
+const lineToNode = (line) => {
+    const match = lineRegex.exec(line)
+    if (!match || line.match(commentRegex)) return null
 
-            const indent = match[1].length
-            return { indent, match, children: [] }
-        })
-        .filter(l => !!l)
+    const indent = match[1].length
+    return {
+        indent,
+        type: match[2],
+        className: match[4],
+        children: []
+    }
+}
+
+const create = (createEl) => {
+    return (templateChunks) => (config) => {
+        const params = [].slice.call(arguments, 1)
+        const lines = templateChunks.join('$$$').split('\n')     // FIXME: join hack
+        const parsedLines = lines.map(lineToNode)
+                                 .filter(l => !!l)
 
         //console.log(parsedLines)
-        const match2dom = (match) => createEl(match[2], { className: match[4] })
-        return match2dom(parsedLines[0].match)
+        const node2dom = (node) => createEl(node.type, { className: node.className })
+        return node2dom(parsedLines[0])
     }
 }
 
